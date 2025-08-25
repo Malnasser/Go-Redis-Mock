4ad40cfcbec7cf553ff2d79f96b63a4dadc18e9b
mhalnasser <alnasser.cs@gmail.com>
2025-08-25 20:58:43 +0300

    handle double pings

--- app/main.go
+++ app/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"net"
 	"os"
@@ -16,19 +17,28 @@ func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
-	// Uncomment this block to pass the first stage
-
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
 
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			continue
+		}
 
-	conn.Write([]byte("+PONG\r\n"))
+		buf := make([]byte, 1021)
+		n, err := conn.Read(buf)
+		if err != nil {
+			fmt.Println("Error reading from client: ", err.Error())
+		}
+
+		pingCount := bytes.Count(buf[:n], []byte("PING"))
+		for i := 0; i < pingCount; i++ {
+			conn.Write([]byte("+PONG\r\n"))
+		}
+	}
 }
